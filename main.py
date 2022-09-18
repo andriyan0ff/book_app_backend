66cb3193da1aa0f90303fb2c78261fbbb1564129
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-18 23:29:36 +0300

    Изменен ендпоинт страны

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     app = Flask(__name__)
     api = Api(app)
     api.add_resource(Users, "/users", "/users/", "/users/<int:id>")
-    api.add_resource(Country, "/users", "/users/", "/country/<int:id>")
+    api.add_resource(Country, "/country", "/country/", "/country/<int:id>")
     app.run(debug=True)
