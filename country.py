08f3d5946a3b2658a33300e4da24877170bd999f
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-18 23:26:08 +0300

    Добавлен метод post, добавление страны

--- country.py
+++ country.py
@@ -42,3 +42,34 @@ class Country(Resource):
             if connection:
                 connection.close()
                 print("[INFO] PostgreSQL connection closed")
+
+    def post(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("name")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM country WHERE name = '""" + str(params["name"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) == 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            """INSERT INTO users (name)
+                            VALUES
+                            ('"""+ str(params["name"]) +"""');""")
+                    return "New country created!", 201
+                else:
+                    return "A country with this name already exists", 409
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
