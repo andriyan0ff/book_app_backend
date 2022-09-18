53e417f6f8ad7d2869e1cdb7b9434c771b08fed6
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-18 22:03:39 +0300

    Добавлен метод post, создание нового пользователя

--- users.py
+++ users.py
@@ -66,7 +66,6 @@ class Users(Resource):
                 print("[INFO] PostgreSQL connection closed")
     def put(self):
         try:
-            jsonData = []
             connection = psycopg2.connect(
                 host=host,
                 user=user,
@@ -107,10 +106,61 @@ class Users(Resource):
                             login = '"""+ str(params["login"]) +"""',
                             password = '"""+ str(params["password"]) +"""'
                              WHERE id = """+ str(params["id"]) +""";""")
-                        #data = cursor.fetchall()
                     return "User id = " + params["id"] + " Updated", 200
                 else:
                     return "User with this id does not exist", 404
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
+
+    def post(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("first_name")
+            parser.add_argument("last_name")
+            parser.add_argument("date_birth")
+            parser.add_argument("email")
+            parser.add_argument("telegram")
+            parser.add_argument("phone")
+            parser.add_argument("country")
+            parser.add_argument("city")
+            parser.add_argument("city_index")
+            parser.add_argument("login")
+            parser.add_argument("password")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM users WHERE login = '""" + str(params["login"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) == 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            """INSERT INTO users (first_name, last_name, date_birth, email, telegram, phone, country, city, city_index, login, password)
+                            VALUES
+                            ('"""+ str(params["first_name"]) +"""',
+                            '"""+ str(params["last_name"]) +"""',
+                            '"""+ str(params["date_birth"]) +"""',
+                            '"""+ str(params["email"]) +"""',
+                            '"""+ str(params["telegram"]) +"""',
+                            '"""+ str(params["phone"]) +"""',
+                            """+ params["country"] +""",
+                            """+ params["city"] +""",
+                            '"""+ str(params["city_index"]) +"""',
+                            '"""+ str(params["login"]) +"""',
+                            '"""+ str(params["password"]) +"""')
+                            ;""")
+                    return "New user created!", 201
+                else:
+                    return "User with this login already exists", 409
         except Exception as ex:
             print("[ERROR] Error while working with PostgreSQL", ex)
         finally:
