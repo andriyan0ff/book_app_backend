93fbd04527c40cf2c57ef345361b7b1ee752ce5f
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-18 23:03:32 +0300

    Добавлен функционал для работы с таблицой country (Страна)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from users import Users
+from country import Country
 
 
 if __name__ == '__main__':
     app = Flask(__name__)
     api = Api(app)
     api.add_resource(Users, "/users", "/users/", "/users/<int:id>")
+    api.add_resource(Country, "/country/<int:id>")
     app.run(debug=True)
