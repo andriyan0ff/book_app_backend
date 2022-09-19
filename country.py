09fa0435b0d1455e82371fe43a8500694b9ae2e0
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-19 19:12:53 +0300

    Добавлен метод put, обновление страны

--- country.py
+++ country.py
@@ -104,3 +104,34 @@ class Country(Resource):
             if connection:
                 connection.close()
                 print("[INFO] PostgreSQL connection closed")
+
+    def put(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("id")
+            parser.add_argument("name")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM country WHERE id = '""" + str(params["id"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) != 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            """UPDATE country SET name = '""" + str(params["name"]) + """' 
+                            WHERE id = '""" + str(params["id"]) + """';""")
+                    return "Country id = " + params["id"] + " Updated", 200
+                else:
+                    return "Country with this id does not exist", 404
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
