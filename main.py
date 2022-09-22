9ffcbf25641463fbdec3345e19b1229e02a07be5
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-22 21:18:25 +0300

    Добавлен ендпоинт authors (Авторы)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from users import Users
 from country import Country
 from city import City
+from authors import Authors
 
 
 if __name__ == '__main__':
@@ -11,4 +12,5 @@ if __name__ == '__main__':
     api.add_resource(Users, "/users", "/users/", "/users/<int:id>")
     api.add_resource(Country, "/country", "/country/", "/country/<int:id>")
     api.add_resource(City, "/city", "/city/", "/city/<int:id>")
+    api.add_resource(Authors, "/authors", "/authors/", "/authors/<int:id>")
     app.run(debug=True)
