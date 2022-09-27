b7fbc79c77b8e9073d92b7a4fa7b45284af05851
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-27 19:23:11 +0300

    Добавил post ендпоинт создания категории

--- category.py
+++ category.py
@@ -42,3 +42,33 @@ class Category(Resource):
             if connection:
                 connection.close()
                 print("[INFO] PostgreSQL connection closed")
+    def post(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("name")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM category WHERE name = '""" + str(params["name"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) == 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            """INSERT INTO category (name)
+                            VALUES
+                            ('"""+ str(params["name"]) +"""');""")
+                    return "New category created!", 201
+                else:
+                    return "A category with this name already exists", 409
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
