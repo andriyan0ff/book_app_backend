4239cc8581cfbda1d321862095a65198062ffcae
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-27 19:54:04 +0300

    добавил delete ендпоинт удаления статуса

--- status.py
+++ status.py
@@ -104,3 +104,34 @@ class Status(Resource):
             if connection:
                 connection.close()
                 print("[INFO] PostgreSQL connection closed")
+
+    def delete(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("id")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM status WHERE id = '""" + str(params["id"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) != 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute("""
+                        UPDATE library SET status = NULL WHERE status = '""" + str(params["id"]) + """';
+                        DELETE FROM status WHERE id = '""" + str(params["id"]) + """';
+                        """)
+                    return "Status id = " + (params["id"]) + " delete!", 200
+                else:
+                    return "Status with this id not found", 404
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
