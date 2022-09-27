a1999de88d2a24f8d5f319ffd4f864c4b88a2e40
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-27 19:29:13 +0300

    Добавил put ендпоинт обновления категории

--- category.py
+++ category.py
@@ -72,3 +72,34 @@ class Category(Resource):
             if connection:
                 connection.close()
                 print("[INFO] PostgreSQL connection closed")
+
+    def put(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("id")
+            parser.add_argument("name")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM category WHERE id = '""" + str(params["id"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) != 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            """UPDATE category SET name = '""" + str(params["name"]) + """' 
+                            WHERE id = '""" + str(params["id"]) + """';""")
+                    return "Category id = " + params["id"] + " Updated", 200
+                else:
+                    return "Category with this id does not exist", 404
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
