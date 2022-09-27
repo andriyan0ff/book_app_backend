501e0cdffcbf7c62fb7cf8c1dbd897371656d167
andriyan0ff <o.andriyanoff@gmail.com>
2022-09-27 19:33:14 +0300

    Добавил delete ендпоинт удаления категории

--- category.py
+++ category.py
@@ -103,3 +103,34 @@ class Category(Resource):
             if connection:
                 connection.close()
                 print("[INFO] PostgreSQL connection closed")
+
+    def delete(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("id")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM category WHERE id = '""" + str(params["id"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) != 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute("""
+                        UPDATE books SET category = NULL WHERE category = '""" + str(params["id"]) + """';
+                        DELETE FROM category WHERE id = '""" + str(params["id"]) + """';
+                        """)
+                    return "Category id = " + (params["id"]) + " delete!", 200
+                else:
+                    return "Category with this id not found", 404
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
