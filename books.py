e0a1105de1725e9f43aaa97775acfc9344c472c4
andriyan0ff <o.andriyanoff@gmail.com>
2022-10-02 10:45:15 +0300

    добавил метод get для ендпоинта книг

---
+++ books.py
@@ -0,0 +1,48 @@
+from flask_restful import Resource, reqparse
+from connect import host, user, password, db_name
+import psycopg2
+
+class Books(Resource):
+    def get(self, id):
+        try:
+            jsonData = []
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            if id == 0:
+                with connection.cursor() as cursor:
+                    cursor.execute("""SELECT * FROM books;""")
+                    data = cursor.fetchall()
+                    for row in data:
+                        result = {}
+                        result["id"] = row[0]
+                        result["name"] = row[1]
+                        result["author"] = row[2]
+                        result["category"] = row[3]
+                        jsonData.append(result)
+                return jsonData, 200
+            if id > 0:
+                with connection.cursor() as cursor:
+                    cursor.execute("""SELECT * FROM books WHERE id = '"""+str(id)+"""';""")
+                    data = cursor.fetchall()
+                    if len(data) != 0:
+                        for row in data:
+                            result = {}
+                            result["id"] = row[0]
+                            result["name"] = row[1]
+                            result["author"] = row[2]
+                            result["category"] = row[3]
+                            jsonData.append(result)
+                        return jsonData, 200
+                    else:
+                        return "id not found", 404
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
