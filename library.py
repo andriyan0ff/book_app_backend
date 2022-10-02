be454b8becdf97d52b426af11cf36a29ab4e1421
andriyan0ff <o.andriyanoff@gmail.com>
2022-10-02 16:43:14 +0300

    вывод книг пользователя

--- library.py
+++ library.py
@@ -15,10 +15,10 @@ class Library(Resource):
             connection.autocommit = True
             parser = reqparse.RequestParser()
             parser.add_argument("visible")
-            parser.add_argument("user")
+            parser.add_argument("users")
             parser.add_argument("book")
             params = parser.parse_args()
-            if str(params["visible"]) == "all" and str(params["user"]) == "" and str(params["book"]) == "":
+            if str(params["visible"]) == "all" and str(params["users"]) == "" and str(params["book"]) == "":
                 with connection.cursor() as cursor:
                     cursor.execute("""SELECT * FROM library;""")
                     data = cursor.fetchall()
@@ -26,10 +26,25 @@ class Library(Resource):
                         result = {}
                         result["id"] = row[0]
                         result["book"] = row[1]
-                        result["user"] = row[2]
+                        result["users"] = row[2]
                         result["status"] = row[3]
                         jsonData.append(result)
                 return jsonData, 200
+            if str(params["visible"]) == "" and str(params["users"]) != "" and str(params["book"]) == "":
+                with connection.cursor() as cursor:
+                    cursor.execute("""SELECT * FROM library WHERE users = '""" + str(params["users"]) + """';""")
+                    data = cursor.fetchall()
+                    if len(data) > 0:
+                        for row in data:
+                            result = {}
+                            result["id"] = row[0]
+                            result["book"] = row[1]
+                            result["users"] = row[2]
+                            result["status"] = row[3]
+                            jsonData.append(result)
+                        return jsonData, 200
+                    else:
+                        return "no users", 404
             else:
                     return "sorry", 400
         except Exception as ex:
