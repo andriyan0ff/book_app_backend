6c05c73b60808d6d70efe6430c1149a448f049cf
andriyan0ff <o.andriyanoff@gmail.com>
2022-10-02 10:59:17 +0300

    добавил метод Post создание книги

--- books.py
+++ books.py
@@ -46,3 +46,36 @@ class Books(Resource):
             if connection:
                 connection.close()
                 print("[INFO] PostgreSQL connection closed")
+
+    def post(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name
+            )
+            connection.autocommit = True
+            parser = reqparse.RequestParser()
+            parser.add_argument("name")
+            parser.add_argument("author")
+            parser.add_argument("category")
+            params = parser.parse_args()
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT * FROM books WHERE name = '""" + str(params["name"]) + """';""")
+                data = cursor.fetchall()
+                if len(data) == 0:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            """INSERT INTO books (name, author, category)
+                            VALUES
+                            ('"""+ str(params["name"]) +"""', '"""+ str(params["author"]) +"""', '"""+ str(params["category"]) +"""');""")
+                    return "New book created!", 201
+                else:
+                    return "A book with this name already exists", 409
+        except Exception as ex:
+            print("[ERROR] Error while working with PostgreSQL", ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
