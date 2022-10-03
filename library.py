4ca64ad662db0ac84e57a4254470e37f7fe66c29
andriyan0ff <o.andriyanoff@gmail.com>
2022-10-03 21:34:28 +0300

    добавлен вывод пользователей по книге

--- library.py
+++ library.py
@@ -43,12 +43,26 @@ class Library(Resource):
                             result["status"] = row[3]
                             jsonData.append(result)
                         return jsonData, 200
+            elif str(params["visible"]) == "" and str(params["users"]) == "" and str(params["book"]) != "":
+                with connection.cursor() as cursor:
+                    cursor.execute("""SELECT * FROM library WHERE book = '""" + str(params["book"]) + """';""")
+                    data = cursor.fetchall()
+                    if len(data) > 0:
+                        for row in data:
+                            result = {}
+                            result["id"] = row[0]
+                            result["book"] = row[1]
+                            result["users"] = row[2]
+                            result["status"] = row[3]
+                            jsonData.append(result)
+                        return jsonData, 200
                     else:
-                        return "no users", 404
+                        return "no books", 404
             else:
                     return "sorry", 400
         except Exception as ex:
             print("[ERROR] Error while working with PostgreSQL", ex)
+            return "ERROR", 400
         finally:
             if connection:
                 connection.close()
