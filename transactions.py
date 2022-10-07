f05218c905f0bcb38ae403ea40a18e685fd8f617
andriyan0ff <o.andriyanoff@gmail.com>
2022-10-07 21:08:40 +0300

    сделал дополнительные проверки в логику

--- transactions.py
+++ transactions.py
@@ -24,27 +24,35 @@ class Transaction(Resource):
             parser.add_argument("book")
             parser.add_argument("st transaction")
             params = parser.parse_args()
-            if params["new user"] != params["old user"]:
-                with connection.cursor() as cursor:
+            if params["new user"] != params["old user"]: #проверка что бы юзер сам себем не сделал трансакцию
+                with connection.cursor() as cursor:#проверка наличия книги у юзера
                     cursor.execute("""
-                                   SELECT login FROM users WHERE id = '"""+ str(params["new user"]) +"""'
+                                   SELECT * FROM library WHERE users = '""" + str(params["old user"]) + """'
+                                   AND book = '""" + str(params["book"]) + """';
                                    """)
-                    newUser = cursor.fetchall()
-                    cursor.execute("""
-                                   SELECT login FROM users WHERE id = '""" + str(params["old user"]) + """'
-                                   """)
-                    oldUser = cursor.fetchall()
-                    cursor.execute("""
-                                   INSERT INTO transactions (new_user, old_user, book, st_transactions)
-                                   VALUES
-                                   ('"""+ str(newUser[0][0]) +"""', '"""+ str(oldUser[0][0]) +"""',
-                                   '"""+ str(params["book"]) +"""', '"""+ str(params["st transaction"]) +"""');
-                                   """)
-                    cursor.execute("""
-                                   UPDATE library SET users = '"""+ str(params["new user"]) +"""',
-                                   status = 1 WHERE users = '""" + str(params["old user"]) + """'
-                                   """)
-                return statusOk, 200
+                    data = cursor.fetchall()
+                    if len(data) != 0: #проверяем что у юзера есть книга с которой он хочет провести трансакцию
+                        cursor.execute("""
+                                       SELECT login FROM users WHERE id = '"""+ str(params["new user"]) +"""';
+                                       """)
+                        newUser = cursor.fetchall()
+                        cursor.execute("""
+                                       SELECT login FROM users WHERE id = '""" + str(params["old user"]) + """';
+                                       """)
+                        oldUser = cursor.fetchall()
+                        cursor.execute("""
+                                       INSERT INTO transactions (new_user, old_user, book, st_transactions)
+                                       VALUES
+                                       ('"""+ str(newUser[0][0]) +"""', '"""+ str(oldUser[0][0]) +"""',
+                                       '"""+ str(params["book"]) +"""', '"""+ str(params["st transaction"]) +"""');
+                                       """)
+                        cursor.execute("""
+                                       UPDATE library SET users = '"""+ str(params["new user"]) +"""',
+                                       status = 1 WHERE users = '""" + str(params["old user"]) + """';
+                                       """)
+                        return statusOk, 200
+                    else:
+                        return statusNo, 400
             else:
                 return statusNo, 400
         except Exception as ex:
